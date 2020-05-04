b46aa7bedcbc7652987182051e4a4bbd980bbc21
GrimesGolden <46364302+GrimesGolden@users.noreply.github.com>
2020-05-04 08:05:14 -0700

    Add files via upload

--- Gladiator.java
+++ Gladiator.java
@@ -8,11 +8,90 @@ public class Gladiator extends Fighter
 	private int special = 3;
 
 	public void specialMove() throws InterruptedException
-	{
-		System.out.println("Special Move Activated\n");
-		special--;
+	{	
+		Random randomNumber = new Random();
+		int x = randomNumber.nextInt(4);
+
+		System.out.println(this.getName() + " looks to the emperor.\n");
+		Thread.sleep(1000);
+		System.out.println("...");
+		Thread.sleep(1000);
+
+		if (this.getName().equals("Player") && this.getSpecial() > 0)
+			// If this turn is  a player turn.
+		{
+			if (Game.computer.getHitpoints() < 25)
+			{
+				if (x == 0)
+				{
+					System.out.println("The emperor looks coldly at the Computer");
+					Thread.sleep(1000);
+					System.out.println("...");
+					Thread.sleep(1000);
+					System.out.println("Thumbs down, FINISH HIM!");
+					Game.computer.decHitpoints(100);
+					System.out.println("In desperation Computer mounts a final attack!!!");
+				}
+
+				else if (x > 0)
+				{
+					System.out.println("The emperor gives a thumbs up!");
+					Thread.sleep(1000);
+					System.out.println("CONTINUE THE FIGHT!");
+				}
+			}
+
+			else if (Game.computer.getHitpoints() > 25)
+			{
+				System.out.println("The emperor does not deign to  intevene, Computers hitpoints are too great.");
+			}
+
+		} // End player if.
+			
+
+		if (this.getName().equals("Computer") && this.getSpecial() > 0)
+		{
+			// If this turn is  a Computer turn.
+			if (Game.player.getHitpoints() < 25)
+			{
+				if (x == 0)
+				{
+					System.out.println("The emperor looks coldly at the Player");
+					Thread.sleep(1000);
+					System.out.println("...");
+					Thread.sleep(1000);
+					System.out.println("Thumbs down, FINISH HIM!");
+					Game.player.decHitpoints(100);
+				}
+
+				else if (x > 0)
+				{
+					System.out.println("The emperor gives a thumbs up!");
+					Thread.sleep(1000);
+					System.out.println("CONTINUE THE FIGHT!");
+					Thread.sleep(1000);
+					System.out.println("...");
+					Thread.sleep(1000);
+					System.out.println("Phew!! That was close!");
+				}
+			}
+
+			else if (Game.player.getHitpoints() > 25)
+			{
+				System.out.println("The emperor does not deign to  intevene, Players hitpoints are too great.");
+			}
+		} // End computer if.
+
+		special--; // Decrement special.
+
+		if (special <= 0) // Special is less than zero. Comes after decrementation to inform player.
+		{
+			System.out.println(" The emperor is bored with the proceedings, do not bother him again!"); 
+			// Therefore out of specials, also sleep so the log doesnt get spammed. 
+			Thread.sleep(1000);
+		}
 		
-	}
+	} // End special move.
 
 	public String getName()
 	{
